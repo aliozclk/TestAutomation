96efb5a9b51e9b9da7a0c8cab3432db6bdcef025
aliozclk <alliozcelik96@gmail.com>
2022-12-21 21:52:52 +0300

    week1

---
+++ selenium-project.java/src/main/java/week_01/exercise01/Demo.java
@@ -0,0 +1,7 @@
+package week_01.exercise01;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
--- selenium-project.java/src/main/java/week_01/exercise01/ListOfAllOrdersPageTest.java
+++ selenium-project.java/src/main/java/week_01/exercise01/ListOfAllOrdersPageTest.java
@@ -6,9 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ListOfAllOrdersPageTest {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\alioz\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\alioz\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        loginPage(driver);
+        checkAllButtonTest(driver);
+        System.out.println("-----------------------");
+
+        loginPage(driver);
+        uncheckAllButtonTest(driver);
+        System.out.println("-------------------------");
+
+
+
+    }
+
+    public static void loginPage(WebDriver driver) throws InterruptedException {
         String username = "Tester";
         String password = "test";
 
@@ -20,7 +33,30 @@ public class ListOfAllOrdersPageTest {
         driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_login_button\"]")).click();
 
         Thread.sleep(2000);
+    }
 
+    public static void checkAllButtonTest(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_btnCheckAll\"]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_btnDelete\"]")).click();
+        Thread.sleep(2000);
+
+        String actual = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_orderMessage\"]")).getText();
+        if (actual.equals("List of orders is empty. In order to add new order use this link.")) {
+            System.out.println("Check All Button Test :  -- Passed --");
+        } else {
+            System.out.println("Check All Button Test :  -- Fail --");
+        }
+    }
+
+    public static void uncheckAllButtonTest(WebDriver driver) throws InterruptedException {
+
+        //Check All
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_btnCheckAll\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_orderGrid_ctl02_OrderSelector\"]")).getAttribute("checked");
+        Thread.sleep(2000);
+
+        //Then uncheck All
 
     }
 }
