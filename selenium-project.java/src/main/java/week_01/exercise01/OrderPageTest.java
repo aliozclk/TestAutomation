67d723d33a744f908d6baba741f0a5bbcf159b34
aliozclk <alliozcelik96@gmail.com>
2022-12-24 16:53:31 +0300

    week1

--- selenium-project.java/src/main/java/week_01/exercise01/OrderPageTest.java
+++ selenium-project.java/src/main/java/week_01/exercise01/OrderPageTest.java
@@ -1,9 +1,15 @@
 package week_01.exercise01;
 
+import org.checkerframework.checker.units.qual.Time;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class OrderPageTest {
     public static void main(String[] args) throws InterruptedException {
@@ -31,13 +37,18 @@ public class OrderPageTest {
 
         //Give a string into the card number line
         invalidFormatInCardNumberBox(driver);
+
+        //Invalid Card Date
+        invalidCardDateTest(driver);
         driver.quit();
 
 
         //Process Button Test
+        processButtonTest(driver);
 
-        //Reset Button Test
+
     }
+
 
     public static void loginPage(WebDriver driver) throws InterruptedException {
         String username = "Tester";
@@ -115,8 +126,8 @@ public class OrderPageTest {
         //check warnings
         String warning = "";
         for (int i = 2; i < 6; i++) {
-            warning = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_RequiredFieldValidator"+ i +"\"]")).getText();
-            if(warning.isEmpty()){
+            warning = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_RequiredFieldValidator" + i + "\"]")).getText();
+            if (warning.isEmpty()) {
                 System.out.println("Address Information Warnings Test : -- Failed --");
                 System.exit(1);
             }
@@ -138,10 +149,9 @@ public class OrderPageTest {
 
         //check that warning
         String warning = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_rev1\"]")).getText();
-        if(warning.equals("Invalid format. Only digits allowed.")){
+        if (warning.equals("Invalid format. Only digits allowed.")) {
             System.out.println("Invalid Format in Zip Box Test : -- Passed --");
-        }
-        else {
+        } else {
             System.out.println("Invalid Format in Zip Box Test : -- Failed --");
         }
 
@@ -155,12 +165,70 @@ public class OrderPageTest {
         Thread.sleep(500);
         //check warning message
         String warning = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_RegularExpressionValidator2\"]")).getText();
-        if(warning.equals("Invalid format. Only digits allowed.")){
+        if (warning.equals("Invalid format. Only digits allowed.")) {
             System.out.println("Invalid Format Warning In Card Box Test : -- Passed --");
-        }
-        else {
+        } else {
             System.out.println("Invalid Format Warning In Card Box Test : -- Failed --");
         }
 
+    }
+
+    private static void processButtonTest(WebDriver driver) throws InterruptedException {
+        loginPage(driver);
+
+        validInfoInputs(driver);
+
+        //click on process
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox1\"]")).click();
+        //go to view all orders page
+        driver.findElement(By.xpath("//*[@id=\"ctl00_menu\"]/li[1]/a")).click();
+        //check infos saved or not
+        if (didInfosSaved(driver)) {
+            System.out.println("Process Button Test : -- Passed --");
+        } else {
+            System.out.println("Process Button Test : -- Passed --");
+        }
+
+    }
+
+    private static boolean didInfosSaved(WebDriver driver) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Date currentDate = new Date();
+
+        String[] infos = {"Customer Name", "MyMoney", "5", formatter.format(currentDate), "Street", "City",
+                "MasterCard", "1234567891234567", "07/25"};
+
+        boolean result = true;
+        for (int i = 2; i < 13; i++) {
+            String xPath = "//*[@id=\"ctl00_MainContent_orderGrid\"]/tbody/tr[2]/td[" + i + "]";
+            String orderInfo = driver.findElement(By.xpath(xPath)).getText();
+            result = result && infos[i].equals(orderInfo);
+        }
+        return result;
+
+    }
+
+    private static void validInfoInputs(WebDriver driver) {
+        //click on order page
+        driver.findElement(By.xpath("//*[@id=\"ctl00_menu\"]/li[3]/a")).click();
+        //give quantity
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_txtQuantity\"]")).sendKeys("5");
+
+        //Address Infos
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_txtName\"]")).sendKeys("Customer Name");
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox2\"]")).sendKeys("Street");
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox3\"]")).sendKeys("City");
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox4\"]")).sendKeys("State");
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox5\"]")).sendKeys("12345");
+
+
+        //click on card type
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_cardList_1\"]")).click();
+
+        //give a card number
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox6\"]")).sendKeys("1234567891234567");
+        //give expire date
+        driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_fmwOrder_TextBox1\"]")).sendKeys("12/25");
     }
 }
